6d7c2b1f43d1d66bf4e116ee036bd05171b5a28e
Harsh Prakash <121473492+workwithharsh@users.noreply.github.com>
2024-08-01 20:54:36 +0530

    Update listings.js

--- controllers/listings.js
+++ controllers/listings.js
@@ -34,8 +34,10 @@ module.exports.showListing = async (req, res) => {
 module.exports.createListing = async (req, res, next) => {
   let url = req.file.path;
   let filename = req.file.filename;
+
   const newListing = new Listing(req.body.listing);
   newListing.owner = req.user._id;
+  newListing.image = { url, filename };
   await newListing.save();
   req.flash("success", "New listing created successfully!");
   res.redirect("/listings");
